970dbf11ba7f12c21da831bc50543cd96e1a93f6
RS <radhe212221@gmail.com>
2022-05-22 12:17:14 +0530

    pass word update

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,5 +1,40 @@
+import axios from 'axios'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 function Orders(props) {
-    return <div>Orders</div>
+    const { orders, user } = useSelector(s => s)
+    const dispatch = useDispatch()
+    const myorders = orders.filter(x => x.uid === user.id)
+
+    return <div className='container'>
+        <h1>total orders ({myorders.length})</h1>
+        <div className='tbl'>
+            <table cellPadding={10} cellSpacing={0}>
+                <thead>
+                    <tr>
+                        <th>sno</th>
+                        <th>photo</th>
+                        <th>qty</th>
+                        <th>price</th>
+                        <th>discount</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {myorders?.map(x => <tr key={x.id}>
+                        <td>{x.id}</td>
+                        <td>
+                            <img width={40} src={x.image} />
+                        </td>
+                        <td>
+                            <strong>{x.qty}</strong>
+                        </td>
+                        <td>{Math.ceil(x.new * x.qty * x.discount / 100)}</td>
+                        <td>{x.discount} % off</td>
+                    </tr>)}
+                </tbody>
+
+            </table></div>
+    </div>
 }
 export default Orders
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,56 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+
+
 function Profile(props) {
-    return <div>Profile</div>
+    const navigate = useNavigate();
+
+
+    const dispatch = useDispatch()
+    const { users, user } = useSelector(s => s)
+    const [ob, setob] = useState(user)
+    const handleChange = e => {
+        let { value, placeholder } = e.target
+        setob({ ...ob, [placeholder]: value })
+    }
+    const handleClick = () => {
+        axios.patch("http://localhost:4000/users/" + ob.id, ob)
+            .then(d => {
+                dispatch({ type: "users", payload: users.map(x => x.id === ob.id ? d.data : x) })
+            })
+            .catch(e => {
+                alert(e.message)
+            })
+    }
+    return <div className='form'>
+        <h1>Profile</h1>
+        <div>
+            <p>name</p>
+            <input value={ob.name} onChange={handleChange} placeholder='name' />
+        </div>
+
+        <div>
+            <p>email</p>
+            <input disabled={true} value={ob.email} onChange={handleChange} placeholder='email' />
+        </div>
+        <div>
+            <p>phone</p>
+            <input disabled={true} value={ob.phone} onChange={handleChange} placeholder='phone' />
+        </div>
+
+
+        <div>
+            <p>password</p>
+            <input value={ob.password} onChange={handleChange} placeholder='password' />
+        </div>
+
+        <div>
+            <button onClick={handleClick}>update</button>
+        </div>
+
+
+    </div>
 }
 export default Profile
